cfdc979a43d6ac441fc34ae65afd41ba664bebfd
Eugene1311 <ef7@mail.ru>
2014-10-26 12:02:26 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 $(function () {
-	$('.header-right img').click(function() {
-		$('#search').animate(
-			{width: "+=250px"}, 200, function () {
-				$('.header-right img').click(function () {
-					$('#search').animate(
-						{width: ""});
-				});	
+	$('.search img').click(function() {
+		$('#search').animate({width: "+=250px"});
 	});
-});
 });
