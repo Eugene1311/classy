a71bb9de099aadf252160ba4174913719afca874
Eugene1311 <ef7@mail.ru>
2014-10-30 23:53:51 +0300

    2

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ $(function () {
 	$('.specials li').hover(function () {
 		$(this).find('a').css('color', '#adadad');//делегирование?
 		$(this).find('h3').css('color', '#fff');
-		$(this).css('background', 'url(../img/menu_top_greenline.png) no-repeat top center');
+		$(this).css({'background': 'url(../img/menu_top_greenline.png) no-repeat top center','backgroundSize':'100%'});
 	}, function () {
 		$(this).find('a').css('color', '#787878');
 		$(this).find('h3').css('color', '#acacac');
