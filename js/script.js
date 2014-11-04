5477ff28f79e2b47f3fd0f43962d5c59fde4f629
Eugene1311 <ef7@mail.ru>
2014-11-05 02:50:16 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 $(function () {
-	$('.search img').click(function() {
-		$('#search')
-			.show('slow')
-			.animate({
-				width : "+=200px"});
-	}).click( function () {
-		$('#search')
-			.animate({
-				width : "-=200px"})
-			.hide('slow');
-	});	//пользовательское событие?			   
-//	$('.search img').click(function() {
-//		$('#search')
-//			.animate({
-//				width : "-=200px"})
-//			.hide('slow');
+//	$('.header-right img').click(function() {
+//		$('#search').animate(
+//			{width: "+=250px"}, 1200, function () {
+//				$('.header-right img').click(function () {
+//					$('#search').animate(
+//						{width: ""});
+//				});	
 //	});
+//});
+	/*$('.specials a').hover(function () {
+		$(this).css('color', '#adadad');//делегирование?
+		$(this).find('h3').css('color', '#fff');
+		$(this).parent().css('background', 'url(../img/menu_top_greenline.png) no-repeat top center');
+	}, function () {
+		$(this).css('color', '#787878');
+		$(this).find('h3').css('color', '#acacac');
+		$(this).parent().css('background', '');
+	});*/
+	$('.specials li').hover(function () {
+		$(this).find('a').css('color', '#adadad');//делегирование?
+		$(this).find('h3').css('color', '#fff');
+		$(this).css({'background': 'url(../img/menu_top_greenline.png) no-repeat top center','backgroundSize':'100%'});
+	}, function () {
+		$(this).find('a').css('color', '#787878');
+		$(this).find('h3').css('color', '#acacac');
+		$(this).css('background', '');
+	});
 });
