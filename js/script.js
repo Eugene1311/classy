90c3670b14174be9e179a7dae40a8c7ad8ce4f22
Eugene1311 <ef7@mail.ru>
2014-11-09 01:56:09 +0300

    2

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@ $(function () {
 	$('.search img').toggle(function() {
 		$('#search')
 			.show()
-			.animate({width: "+=200px"}, 1200)
+			.animate({width: "+=200px"}, 1000)
 		},function () {
 		$('#search')
-			.animate({width: "-=200px"},1200)
+			.animate({width: "-=200px"},1000)
 	});	
-	$('.specials li').hover(function () {
-		$(this).find('a').css('color', '#adadad');//делегирование?
-		$(this).find('h3').css('color', '#fff');
-		$(this).css({'background': 'url(../img/menu_top_greenline.png) no-repeat top center','backgroundSize':'100%'});
-	}, function () {
-		$(this).find('a').css('color', '#787878');
-		$(this).find('h3').css('color', '#acacac');
-		$(this).css('background', '');
-	});
+//	$('.specials li').hover(function () {
+//		$(this).find('a').css('color', '#adadad');//делегирование?
+//		$(this).find('h3').css('color', '#fff');
+//		$(this).addClass('hovered');
+//	}, function () {
+//		$(this).find('a').css('color', '#787878');
+//		$(this).find('h3').css('color', '#acacac');
+//		$(this).removeClass('hovered');
+//	});
 });
